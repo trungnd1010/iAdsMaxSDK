1a4d44ef906051475ac598dea187c0d01f99e490
TrungCrit <trungnd@ikameglobal.com>
2024-11-04 14:18:57 +0700

    [add] setup sdk

--- iAdsMaxSDKManager.swift
+++ iAdsMaxSDKManager.swift
@@ -5,4 +5,19 @@
 //  Created by Nguyễn Trung on 4/11/24.
 //
 
-import Foundation
+import AppLovinSDK
+
+public class iAdsMaxSDKManager {
+    public static let shared = iAdsMaxSDKManager()
+    private init() {}
+    
+    public func setup(sdkKey: String, isTestAds: Bool) {
+        let initConfig = ALSdkInitializationConfiguration(sdkKey: sdkKey) { builder in
+            builder.mediationProvider = ALMediationProviderMAX
+            if isTestAds, let currentIDFV = UIDevice.current.identifierForVendor?.uuidString {
+                builder.testDeviceAdvertisingIdentifiers = [currentIDFV]
+            }
+        }
+        ALSdk.shared().initialize(with: initConfig) { _ in }
+    }
+}
